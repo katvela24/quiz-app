5f90182a6f4152e9571b3aeefc64de1b09e772ca
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-02-14 17:02:47 -0500

    added variables to the js file and added css link to the HTML file

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// Global Variables (the variables that need to be everywhere i.e. score variable, array of questions, timer variable)
+var startscreenEl = document.getElementById("startscreen")
+var quizquestionsEl = document.getElementById("quizquestions")
+var questionEl = document.getElementById("question")
+var answersEl = document.getElementById("answers")
+
+var questions=[
+            
+    {
+        question: "What is the ideal data structure for storing a list of items",
+        answers: ["variables", "function", "array", "ids"],
+        correctanswer: 2
+    },
+    {
+        question: "What is the ideal data structure for storing a list of items",
+        answers: ["variables", "function", "array", "ids"],
+        correctanswer: 2
+    },
+    {
+        question: "What is the ideal data structure for storing a list of items",
+        answers: ["variables", "function", "array", "ids"],
+        correctanswer: 2
+    },
+    {
+        question: "What is the ideal data structure for storing a list of items",
+        answers: ["variables", "function", "array", "ids"],
+        correctanswer: 2
+    },
+    {
+        question: "What is the ideal data structure for storing a list of items",
+        answers: ["variables", "function", "array", "ids"],
+        correctanswer: 2
+    },
+    
+]
+
+
+
+// Functions (reusable blocks of code)
+
+
+
+// Main process (where you call the first couple of functions)
+
+document.getElementById("startbutton").addEventListener("click",function(){
+alert("testing")
+})
+
+                                                            
