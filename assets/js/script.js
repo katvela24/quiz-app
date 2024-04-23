2e5ffecd2f438729c9def0bf07b0a6ff59403266
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-04-23 10:39:01 -0400

    captured input for initials

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 // Global Variables (the variables that need to be everywhere i.e. score variable, array of questions, timer variable)
 var startscreenEl = document.getElementById("startscreen")
 var quizquestionsEl = document.getElementById("quizquestions")
+var endscreenEl = document.getElementById("end-screen")
 var questionEl = document.getElementById("question")
 var answersEl = document.getElementById("answers")
+var highscoreEl = document.getElementById("high-score")
+
 var questionindex = 0
 var score = 0
 var numSeconds = 60;
 var timerEl = document.getElementById("timer")
+let interval;
 
-var questions=[
-            
+var questions = [
+
     {
         question: "What is the ideal data structure for storing a list of items?",
         answers: ["variable", "function", "array", "ids"],
@@ -42,10 +46,10 @@ var questions=[
 
 // Functions (reusable blocks of code)
 
-function startGame () {
+function startGame() {
     // Show the start screen
-    startscreenEl.setAttribute("class" , "show")
-    
+    startscreenEl.setAttribute("class", "show")
+
     // Hide the question part
     quizquestionsEl.setAttribute("class", "hide")
 
@@ -55,9 +59,9 @@ function startGame () {
     // Reset the score at the end of the game
     score = 0
 
-      // Reset the timer seconds
+    // Reset the timer seconds
     // numSeconds = secondsPerQuestion;
-    }   
+}
 
 // asks an individual question
 function askquestion() {
@@ -67,59 +71,79 @@ function askquestion() {
     questionEl.setAttribute("data-index", questionindex)
     // when a question is asked, throws away the previous question's answers before outputting the new question's answers
     answersEl.innerText = ""
-    
-    for (var i = 0; i<currentAnswers.length; i++) {
-    
-        var button =document.createElement ("button")
-    
+
+    for (var i = 0; i < currentAnswers.length; i++) {
+
+        var button = document.createElement("button")
+
         button.innerText = currentAnswers[i]
-        button.setAttribute("value", currentAnswers [i])
-        button.onclick = checkanswer
+        button.setAttribute("value", currentAnswers[i])
         answersEl.appendChild(button)
-    } 
+        button.onclick = checkanswer
+
+    }
     // questionindex++
 }
-function checkanswer () {
-    if(this.value === questions[questionindex].correctanswer) {
-        console.log ("correct!")
-    } else{console.log ("incorrect")
-    numSeconds = numSeconds-10
-    timerEl.textContent = numSeconds
-} 
+function checkanswer() {
+    console.log("index", questionindex);
+    console.log("length", questions.length - 1);
+
+    if (this.value === questions[questionindex].correctanswer) {
+        console.log("correct!")
+    } else {
+        console.log("incorrect")
+        numSeconds = numSeconds - 10
+        timerEl.textContent = numSeconds
+    }
     questionindex++
     askquestions()
 }
-function askquestions () {
-    
+
+function endGame() {
+    endscreenEl.setAttribute("class", "show")
+
+    quizquestionsEl.setAttribute("class", "hide")
+
+}
+function askquestions() {
+
     // kicks off the first question
-      askquestion();
+    askquestion();
+
 
     //   start an interval to then ask the rest of the questions at a delay
-    var interval = setInterval(function(){
-        numSeconds = numSeconds -1
+    interval = setInterval(function () {
+        numSeconds = numSeconds - 1
         timerEl.textContent = numSeconds
-        if (questionindex > questions.length - 1){
-            clearInterval (interval)
-            alert ("done")
+        if (questionindex === questions.length) {
+            clearInterval(interval)
+            timerEl.textContent = 0
+            return endGame();
+            // alert ("done")
         }
+
     }, 1000)
 
 
 }
 // Main process (where you call the first couple of functions)
 
-document.getElementById("startbutton").addEventListener("click",function(){
-    startscreenEl.setAttribute("class" , "hide")
-    
+document.getElementById("startbutton").addEventListener("click", function () {
+    startscreenEl.setAttribute("class", "hide")
+
     quizquestionsEl.setAttribute("class", "show")
     askquestions()
 })
 
-function startGame () {
- 
+document.getElementById("submit").addEventListener("click", function () {
+    let initialsEL = document.getElementById("initials")
+    endscreenEl.setAttribute("class", "hide")
+    console.log("testing",initialsEL.value);
+    console.log("testing2",initialsEL.innerHTML);
+    highscoreEl.setAttribute("class", "show")
+})
+
+function startGame() {
+
 }
 
-function endGame () {
-
-
-}                                                 
